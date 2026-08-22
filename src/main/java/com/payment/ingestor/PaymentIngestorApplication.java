387dd083ac61c833e805ca089521e0a67445a7d7
Vinod Kumar Yadav <vinodky641@gmail.com>
2026-08-22 17:35:46 +0530

    Added config properties and enabled scheduling

--- src/main/java/com/payment/ingestor/PaymentIngestorApplication.java
+++ src/main/java/com/payment/ingestor/PaymentIngestorApplication.java
@@ -2,7 +2,9 @@ package com.payment.ingestor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class PaymentIngestorApplication {
 
